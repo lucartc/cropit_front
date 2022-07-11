5b584901ce8efb19a4339076e000b582e4f177c7
jlucartc <jlucartc@gmail.com>
2022-07-11 18:15:06 -0300

    Fixing test cases

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -7,22 +7,23 @@ describe('Test suit',() => {
     cy.get('#import-image').should('not.exist')
   })
 
-  it('Should update current-div image when a new aspect-ratio is created',() => {
+  it('Should update crop-window when a new aspect-ratio is created',() => {
     cy.visit('/app')
+    cy.get('#import-image-input').selectFile('cypress/fixtures/flower.jpeg',{force: true})
     cy.get('#add-aspect-ratio').click()
     cy.get('input[name=width]').type('{backspace}3')
     cy.get('input[name=height]').type('{backspace}4')
     cy.get('#confirm-button').click()
-    cy.get('#current-image').should('have.css','aspect-ratio','3 / 4')
+    cy.get('#crop-window').should('have.css','aspect-ratio','3 / 4')
   })
 
   it('Should show tooltips when hovering buttons',() => {
     expect(true).to.be.false
   })
 
-  it('Should show next image by clicking the next button if there are at least 2 aspect ratios',() => {
+  it('Should show next crop-window aspect ratio by clicking the next button if there are at least 2 aspect ratios',() => {
     cy.visit('/app')
-    cy.get('#current-image').should('have.css','aspect-ratio','2 / 1')
+    cy.get('#import-image-input').selectFile('cypress/fixtures/flower.jpeg',{force: true})
     cy.get('#add-aspect-ratio').click()
     cy.get('input[name=width]').type('{backspace}1')
     cy.get('input[name=height]').type('{backspace}2')
@@ -31,12 +32,12 @@ describe('Test suit',() => {
     cy.get('input[name=height]').type('{backspace}4')
     cy.get('#confirm-button').click()
     cy.get('#next').click()
-    cy.get('#current-image').should('have.css','aspect-ratio','3 / 4')
+    cy.get('#crop-window').should('have.css','aspect-ratio','3 / 4')
   })
 
-  it('Should show previous image by clicking the previous button if there are at least 2 aspect ratios',() => {
+  it('Should show previous crop-window aspect ratio by clicking the previous button if there are at least 2 aspect ratios',() => {
     cy.visit('/app')
-    cy.get('#current-image').should('have.css','aspect-ratio','2 / 1')
+    cy.get('#import-image-input').selectFile('cypress/fixtures/flower.jpeg',{force: true})
     cy.get('#add-aspect-ratio').click()
     cy.get('input[name=width]').type('{backspace}3')
     cy.get('input[name=height]').type('{backspace}4')
@@ -45,15 +46,16 @@ describe('Test suit',() => {
     cy.get('input[name=height]').type('{backspace}2')
     cy.get('#confirm-button').click()
     cy.get('#previous').click()
-    cy.get('#current-image').should('have.css','aspect-ratio','1 / 2')
+    cy.get('#crop-window').should('have.css','aspect-ratio','1 / 2')
   })
 
   it('Should display first JSON aspect ratio when JSON is used',() => {
     cy.visit('/app')
+    cy.get('#import-image-input').selectFile('cypress/fixtures/flower.jpeg',{force: true})
     cy.get('#import-json').click()
     cy.get('#json-container').type('{"ratios":[{"width":1,"height":2},{"width":3,"height":4}]}',{ parseSpecialCharSequences: false })
     cy.get('.aspect-ratio-item-active').first().should('have.text','1:2')
-    cy.get('#current-image').should('have.css','aspect-ratio','1 / 2')
+    cy.get('#crop-window').should('have.css','aspect-ratio','1 / 2')
   })
 
   it('Should display error message above JSON textarea if JSON is invalid',() => {
