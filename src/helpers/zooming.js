f412088e464d108e454cf930ee4b785ede0a4a8d
jlucartc <jlucartc@gmail.com>
2022-07-17 19:55:55 -0300

    Refactoring zooming module

--- src/helpers/zooming.js
+++ src/helpers/zooming.js
@@ -3,10 +3,12 @@ import { ref, computed, watch } from "vue";
 const image_current_position = ref(null)
 const image_current_size = ref(null)
 const zoom_percentage = 5
+const zoom_container = ref(null)
 
 function zoom(cursor_absolute_position,container,in_out) {
 	const background_image = new Image()
   const container_computed_style = getComputedStyle(container)
+  zoom_container.value = container
 
   background_image.src = container_computed_style.backgroundImage
   											 .replace('url("','')
@@ -14,75 +16,47 @@ function zoom(cursor_absolute_position,container,in_out) {
 
   background_image.addEventListener('load',(event) => {
   	if(image_current_size.value == null){
-  		convert_background_image_dimensions_to_pixels(background_image,container,in_out)
+  		convert_background_image_dimensions_to_pixels(background_image)
   	}
-  	calculate_zooming(cursor_absolute_position,container,in_out)
+  	calculate_zooming(cursor_absolute_position,in_out)
   })
 }
 
-function convert_background_image_dimensions_to_pixels(background_image,container,in_out){
-	const container_computed_style = getComputedStyle(container)
+function convert_background_image_dimensions_to_pixels(background_image){
+	const container_computed_style = getComputedStyle(zoom_container.value)
 	let background_size = container_computed_style.backgroundSize
-	let current_dimensions = null
+	let image_dimensions = null
 
 	switch(background_size){
     case "contain":
-      current_dimensions = calculate_containing_image_position(background_image,container,in_out)
+      image_dimensions = calculate_containing_image_dimensions(background_image)
       break;
     case "cover":
-      current_dimensions = calculate_covering_image_position(background_image,container,in_out)
+      image_dimensions = calculate_covering_image_dimensions(background_image)
       break;
 	}
 
-	image_current_size.value = {x: current_dimensions.image_width, y: current_dimensions.image_height}
-	image_current_position.value = {x: current_dimensions.left, y: current_dimensions.top}
-	container.style.backgroundSize = `${current_dimensions.image_width}px ${current_dimensions.image_height}px`
-	container.style.backgroundPosition = `${current_dimensions.left}px ${current_dimensions.top}px`
+	image_current_size.value = {
+		x: image_dimensions.image_width,
+		y: image_dimensions.image_height
+	}
+	
+	image_current_position.value = {
+		x: image_dimensions.left,
+		y: image_dimensions.top
+	}
+
+	let style = zoom_container.value.style
+	let width = image_dimensions.image_width
+	let height = image_dimensions.image_height
+	let distance_top = image_dimensions.top
+	let distance_left = image_dimensions.left
+	style.backgroundSize = `${width}px ${height}px`
+	style.backgroundPosition = `${distance_left}px ${distance_top}px`
 }
 
-function calculate_centered_image_position(image, container,in_out) {
-  const container_box = container.getBoundingClientRect();
-  const image_width = image.naturalWidth;
-  const image_height = image.naturalHeight;
-
-  let rendered_width = null;
-  let rendered_height = null;
-
-  if (image_width >= image_height) {
-    rendered_width = container_box.width;
-    rendered_height = container_box.width * (1 / image_aspect_ratio(image));
-  } else {
-    rendered_height = container_box.height;
-    rendered_width = container_box.height * image_aspect_ratio(image);
-  }
-
-  let left = null;
-  let top = null;
-
-  if (image_width >= image_height) {
-    if (container_box.width >= container_box.height) {
-      left = (container_box.width - image_width) / 2;
-    } else {
-      top = (container_box.height - image_height) / 2;
-    }
-  } else {
-    if (container_box.width >= container_box.height) {
-      left = (container_box.width - image_width) / 2;
-    } else {
-      top = (container_box.height - image_height) / 2;
-    }
-  }
-
-  return {
-    image_width: rendered_width,
-    image_height: rendered_height,
-    left: left,
-    top: top
-  };
-}
-
-function calculate_covering_image_position(image,container,in_out) {
-  const container_box = container.getBoundingClientRect();
+function calculate_covering_image_dimensions(image) {
+  const container_box = zoom_container.value.getBoundingClientRect();
   const container_area = container_box.width.container_box.height;
   const width_aligned_area = container_box.width ** 2 * (1 / image_aspect_ratio(image));
   const height_aligned_area = container_box.height ** 2 * image_aspect_ratio(image);
@@ -97,8 +71,8 @@ function calculate_covering_image_position(image,container,in_out) {
     image_height = container_box.width * (1 / image_aspect_ratio(image));
   }
 
-  const left = Math.abs(image_width - container_box.width) / 2;
   const top = Math.abs(image_height - container_box.height) / 2;
+  const left = Math.abs(image_width - container_box.width) / 2;
 
   return {
     image_height: image_height,
@@ -108,27 +82,27 @@ function calculate_covering_image_position(image,container,in_out) {
   };
 }
 
-function calculate_containing_image_position(image,container,in_out) {
-  const container_box = container.getBoundingClientRect();
-  const container_computed_style = getComputedStyle(container);
+function calculate_containing_image_dimensions(image) {
+  const container_box = zoom_container.value.getBoundingClientRect();
+  const container_computed_style = getComputedStyle(zoom_container.value);
   const position = container_computed_style.backgroundPosition;
   const distance_x_percentage = parseFloat(position.split("%", 2).shift());
   const distance_y_percentage = parseFloat(position.split("%", 2).pop().trim());
   let image_width = null;
   let image_height = null;
-  const distance_x_pixels = (container_box.width * distance_x_percentage)/100;
-  const distance_y_pixels = (container_box.height * distance_y_percentage)/100;
+  const distance_x_pixels = (container_box.width * distance_x_percentage) / 100;
+  const distance_y_pixels = (container_box.height * distance_y_percentage) / 100;
 
   if (image.naturalWidth > image.naturalHeight) {
     image_width = container_box.width;
-    image_height = image_width * (1/image_aspect_ratio(image));
+    image_height = image_width * (1 / image_aspect_ratio(image));
   } else {
     image_height = container_box.height;
     image_width = image_height * image_aspect_ratio(image);
   }
 
-  const top = distance_y_pixels - image_height/2;
-  const left = distance_x_pixels - image_width/2;
+  const top = distance_y_pixels - image_height / 2;
+  const left = distance_x_pixels - image_width / 2;
 
   return {
     image_height: image_height,
@@ -144,12 +118,12 @@ function image_aspect_ratio(image) {
   return parseFloat(width) / parseFloat(height);
 }
 
-function calculate_zooming(cursor_absolute_position,container,in_out){
-  const container_computed_style = getComputedStyle(container)
+function calculate_zooming(cursor_absolute_position,in_out){
+  const container_computed_style = getComputedStyle(zoom_container.value)
   let resized_dimensions = null
   let background_size = null
 
-  resized_dimensions = resize_dimensions(cursor_absolute_position,container,in_out)
+  resized_dimensions = resize_dimensions(cursor_absolute_position,in_out)
 
 	image_current_size.value = {
 		x: resized_dimensions.image_width,
@@ -161,12 +135,18 @@ function calculate_zooming(cursor_absolute_position,container,in_out){
 		y: resized_dimensions.top
 	}
 
-  container.style.backgroundSize = `${image_current_size.value.x}px ${image_current_size.value.y}px`;
-  container.style.backgroundPosition = `${image_current_position.value.x}px ${image_current_position.value.y}px`;
+	let style = zoom_container.value.style
+	let new_width = resized_dimensions.image_width
+	let new_height = resized_dimensions.image_height
+	let new_distance_top = resized_dimensions.top
+	let new_distance_left = resized_dimensions.left
+  style.backgroundSize = `${new_width}px ${new_height}px`;
+  style.backgroundPosition = `${new_distance_left}px ${new_distance_top}px`;
 }
 
-function resize_dimensions(cursor_absolute_position,container,in_out){
-	let container_box = container.getBoundingClientRect()
+function resize_dimensions(cursor_absolute_position,in_out){
+	let container_box = zoom_container.value.getBoundingClientRect()
+	
 	let dimensions = {
 		image_width: image_current_size.value.x,
 		image_height: image_current_size.value.y,
@@ -178,8 +158,8 @@ function resize_dimensions(cursor_absolute_position,container,in_out){
   dimensions.image_height *= scaling_factor(in_out)
 
   let image_center = {
-  	x: dimensions.left + dimensions.image_width/2,
-  	y: dimensions.top + dimensions.image_height/2
+  	x: dimensions.left + dimensions.image_width / 2,
+  	y: dimensions.top + dimensions.image_height / 2
   }
 
   let cursor_relative_position = {
@@ -192,28 +172,28 @@ function resize_dimensions(cursor_absolute_position,container,in_out){
     y: (image_center.y - cursor_relative_position.y) * scaling_factor(in_out)
   }
 
-	let bottom = Math.abs(new_image_to_cursor_distance.y) - Math.abs(dimensions.image_height/2) + Math.abs(container_box.bottom - cursor_absolute_position.y)
-	let right = Math.abs(new_image_to_cursor_distance.x) - Math.abs(dimensions.image_width/2) + Math.abs(container_box.right - cursor_absolute_position.x)
+	let bottom = Math.abs(new_image_to_cursor_distance.y) - Math.abs(dimensions.image_height / 2) + Math.abs(container_box.bottom - cursor_absolute_position.y)
+	let right = Math.abs(new_image_to_cursor_distance.x) - Math.abs(dimensions.image_width / 2) + Math.abs(container_box.right - cursor_absolute_position.x)
 
 	if(image_is_above_left(new_image_to_cursor_distance)){
 		dimensions.top = Math.abs(container_box.height) - Math.abs(bottom) - Math.abs(dimensions.image_height)
 		dimensions.left = Math.abs(container_box.width) - Math.abs(right) - Math.abs(dimensions.image_width)
 	}else if(image_is_above_right(new_image_to_cursor_distance)){
 		dimensions.top = Math.abs(container_box.height) - Math.abs(bottom) - Math.abs(dimensions.image_height)
-		dimensions.left = Math.abs(new_image_to_cursor_distance.x) - Math.abs(dimensions.image_width/2) + Math.abs(cursor_relative_position.x)
+		dimensions.left = Math.abs(new_image_to_cursor_distance.x) - Math.abs(dimensions.image_width / 2) + Math.abs(cursor_relative_position.x)
 	}else if(image_is_below_left(new_image_to_cursor_distance)){
-		dimensions.top = Math.abs(new_image_to_cursor_distance.y) - Math.abs(dimensions.image_height/2) + Math.abs(cursor_relative_position.y)
+		dimensions.top = Math.abs(new_image_to_cursor_distance.y) - Math.abs(dimensions.image_height / 2) + Math.abs(cursor_relative_position.y)
 		dimensions.left = Math.abs(container_box.width) - Math.abs(right) - Math.abs(dimensions.image_width)
 	}else if(image_is_below_right(new_image_to_cursor_distance)){
-		dimensions.top = Math.abs(new_image_to_cursor_distance.y) - Math.abs(dimensions.image_height/2) + Math.abs(cursor_relative_position.y)
-		dimensions.left = Math.abs(new_image_to_cursor_distance.x) - Math.abs(dimensions.image_width/2) + Math.abs(cursor_relative_position.x)
+		dimensions.top = Math.abs(new_image_to_cursor_distance.y) - Math.abs(dimensions.image_height / 2) + Math.abs(cursor_relative_position.y)
+		dimensions.left = Math.abs(new_image_to_cursor_distance.x) - Math.abs(dimensions.image_width / 2) + Math.abs(cursor_relative_position.x)
 	}
 
 	return dimensions
 }
 
 function scaling_factor(in_out){
-	return in_out == 'in' ? Math.sqrt(1 + zoom_percentage/100) : 1/Math.sqrt(1 + zoom_percentage/100)
+	return in_out == 'in' ? Math.sqrt(1 + zoom_percentage / 100) : 1 / Math.sqrt(1 + zoom_percentage / 100)
 }
 
 function image_is_left(distance){
